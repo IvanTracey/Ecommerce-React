c9613114a770952128e605b71cf87735c9adb036
IvoCode <ivtracey@gmail.com>
2026-08-13 15:16:43 -0300

    Creacion

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+import Header from './Header/Header'
+import { Footer } from './Footer/Footer'
+
+function App() {
+  
+  return (
+    <>
+    <Header></Header>
+    <h1>App</h1>
+    <Footer></Footer>
+    </>
+  )
+}
+
+export default App
