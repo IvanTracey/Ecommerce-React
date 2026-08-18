9ff775f1c03d16acd5ca1bcea79c00638de39061
IvoCode <ivtracey@gmail.com>
2026-08-18 14:06:38 -0300

    Mas info en card

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,13 +1,36 @@
+import { useState } from 'react';
 import ItemList from '../Components/ItemList'
 import './Card.css'
 
 // Creo la estructura de la Card y designo con que informacion la relleno
 function Card({ producto }) {
+  const [contador, setContador]= useState(0);
+
+  const incrementarCont = () => {
+    setContador(contador + 1);
+  };
+  const decrementarCont = () => {
+    if(contador!=0){
+      setContador(contador - 1);
+    }
+  };
   return (
     <div className="card">
       <img className='card-img' src={producto.img} alt={producto.nombre} />
-      <h3>{producto.nombre}</h3>
-      <p>${producto.precio}</p>
+      <div className="div-nombre">
+        <h3>{producto.nombre}</h3>
+      </div>
+      <div className="div-descripcion">
+        <p>{producto.descripcion}</p>
+      </div>
+      <div className='div-precio'>
+        <h4>${producto.precio}</h4>
+      </div>
+      <div className='div-botonera'>
+        <button className='boton' onClick={decrementarCont}>-</button>
+        <p>{contador}</p>
+        <button className='boton' onClick={incrementarCont}>+</button>
+      </div>
     </div>
   );
 }
