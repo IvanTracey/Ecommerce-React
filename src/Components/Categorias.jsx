e9cc094e801b0948482d444a566f2a66b57a6591
IvoCode <ivtracey@gmail.com>
2026-08-21 12:24:16 -0300

    Funciona el filtrado

--- src/Components/Categorias.jsx
+++ src/Components/Categorias.jsx
@@ -1,22 +1,26 @@
 import Button from '@mui/material/Button';
 import './Categorias.css'
 
-export default function ContainedButtons() {
+function CategorySelection({ categoriaActiva, onSelectCategoria }) {
     const categorias = ["Todos", "Adornos", "Aromas", "Defumación", "Manifestación", "Piedras", "Portasahumerios", "Portavelas", "Velas"];
 
   return (
     <div className='categorias'>
       {categorias.map(categoria => (  
-        <Button key={categoria} className="botones-cat" variant="contained" 
-            sx={{
-                bgcolor: '#A8B8A5', 
-                color: '#000000', 
-                fontWeight: 'bold',
-                '&:hover': {
-                bgcolor: '#7cc26f',
-                },
+        <Button 
+          key={categoria} 
+          onClick={() => onSelectCategoria(categoria)} className="botones-cat" variant="contained" 
+          sx={{
+              bgcolor: categoria === categoriaActiva ? '#5a9d51' : '#A8B8A5', 
+              color: '#000000', 
+              fontWeight: 'bold',
+              '&:hover': {
+                bgcolor: '#5a9d51',
+              },
         }}>{categoria}</Button>
       ))}
     </div>
   );
 }
+
+export default CategorySelection;
--- src/Pages/Catalogo.jsx
+++ src/Pages/Catalogo.jsx
@@ -1,18 +1,21 @@
-import React  from 'react'
+import { useState } from "react"
 import ItemListContainer from '../Components/ItemListContainer'
 import Categorias from '../Components/Categorias'
 import './Catalogo.css'
 
 function Catalogo (){
-  
+  const [categoriaSeleccionada, setCategoriaSeleccionada] = useState("Todos");
+
   return (
     <div className='div-catalogo'>
         <div className='div-categorias'>
-            <Categorias></Categorias>
+          <Categorias             
+            categoriaActiva={categoriaSeleccionada}
+            onSelectCategoria={setCategoriaSeleccionada}
+          />
         </div>
         <div className='div-cardproducts'>
-             <ItemListContainer></ItemListContainer>
-      
+          <ItemListContainer categoriaSeleccionada={categoriaSeleccionada} />
         </div>
     </div>
   )
