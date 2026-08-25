13778f0f65c22a111a818cea826957234dac29b9
IvoCode <ivtracey@gmail.com>
2026-08-25 18:44:26 -0300

    Botonera de carrito

--- src/Pages/Carrito.jsx
+++ src/Pages/Carrito.jsx
@@ -5,24 +5,25 @@ import CardCarrito from '../Components/CardCarrito';
 import { Link } from 'react-router-dom';
 
 function Carrito() {
-  const {cart} = useContext(CartContext)
+  const {cart, vaciarCarrito} = useContext(CartContext)
   const cantidadItems = cart.reduce((total, item) => total + item.cantidad,0);
   const montoTotal = cart.reduce((total, item) => total + item.precio * item.cantidad, 0);
 
-  const finalizarCompra = () => {
-    
-  }
   return (
     <div className='carrito'>
       <div className="header-carrito">
         <h2>Carrito</h2>
-        {cantidadItems ? <p>TOTAL A PAGAR ${montoTotal} - {cantidadItems} items</p> : <p>Vacío</p>}
+        {cantidadItems ? <p>TOTAL A PAGAR ${montoTotal} ({cantidadItems} items)</p> : <p>(Vacío)</p>}
       </div>
-      <hr />
       <div className="body-carrito">
           <CardCarrito></CardCarrito>
       </div>
-      {cantidadItems ? <Link to={`../checkout`}><button>Finalizar compra</button></Link> : ""}
+      {cantidadItems ? 
+        <div className='div-botones'>
+          <Link to={`/`}><button>Seguir comprando</button></Link>
+          <button onClick={vaciarCarrito}>Vaciar carrito</button>
+          <Link to={`../checkout`}><button>Finalizar compra</button></Link>
+        </div> : "" }
     </div>
     
   )
